406084d78abb12cedfb808d95acfd44b87bf3780
Rishan-Jadva <rishanjadva@gmail.com>
2025-05-11 15:34:07 +1000

    Created cell-to-cell drawable lines

--- cell.py
+++ cell.py
@@ -29,3 +29,23 @@ class Cell:
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self.__win.draw_line(line)
+        
+    def draw_move(self, to_cell, undo=False):
+        len_x = max(self.__x1, self.__x2) - min(self.__x1, self.__x2)
+        len_y = max(self.__y1, self.__y2) - min(self.__y1, self.__y2)
+        x_center = len_x // 2 + min(self.__x1, self.__x2)
+        y_center = len_y // 2 + min(self.__y1, self.__y2)
+
+        len_x2 = max(to_cell.__x1, to_cell.__x2) - min(to_cell.__x1, to_cell.__x2)
+        len_y2 = max(to_cell.__y1, to_cell.__y2) - min(to_cell.__y1, to_cell.__y2)
+        x_center2 = len_x2 // 2 + min(to_cell.__x1, to_cell.__x2)
+        y_center2 = len_y2 // 2 + min(to_cell.__y1, to_cell.__y2)
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+
+        if undo:
+            color = "gray"
+        else:
+            color = "red"
+
+        self.__win.draw_line(line, color)
--- main.py
+++ main.py
@@ -4,20 +4,16 @@ from cell import Cell
 def main():
     win = Window(800, 600)
     c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
-
-    c = Cell(win)
     c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c.draw(100, 200, 300, 400)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.draw(300, 200, 500, 400)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    c.draw_move(c2)
+
+    win.wait_for_close()
 
 if __name__ == "__main__":
     main()
