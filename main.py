c09cb9aab102c23ba9c4342a39e6ccc587798231
Rishan-Jadva <rishanjadva@gmail.com>
2025-05-11 14:32:25 +1000

    implemented line drawing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from window import Window
+from window import Window, Line, Point
 
-def Main():
+def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(50,50), Point(400,400)), "black")
     win.wait_for_close()
+
+if __name__ == "__main__":
+    main()
